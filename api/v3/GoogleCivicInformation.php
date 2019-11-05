4e79dfcbd8e372c5d66a4783b8c65b51a2d6f8ed
Allen Shaw <allen@JoineryHQ.com>
2019-11-05 13:49:21 -0600

    Added extra logging on address API errors.

--- api/v3/GoogleCivicInformation.php
+++ api/v3/GoogleCivicInformation.php
@@ -73,7 +73,7 @@ function google_civic_information_country_districts($level, $limit, $update) {
     //Check for errors first
     if ( isset($districts['error']) ) {
       $addressesWithErrors++;
-      electoral_district_address_errors($districts, $contactAddresses->id);
+      electoral_district_address_errors($districts, $contactAddresses->id, $url);
     //Process divisions
     } else {
       $countryDivision = strtolower("ocd-division/country:us/state:$stateProvinceAbbrev");
@@ -146,7 +146,7 @@ function google_civic_information_state_districts($level, $limit, $update) {
     //Check for errors first
     if ( isset($districts['error']) ) {
       $addressesWithErrors++;
-      electoral_district_address_errors($districts, $contactAddresses->id);
+      electoral_district_address_errors($districts, $contactAddresses->id, $url);
     //Process divisions
     } else {
       $countryDivision = strtolower("ocd-division/country:us/state:$stateProvinceAbbrev");
@@ -217,7 +217,7 @@ function google_civic_information_county_districts($level, $limit, $update) {
     //Check for errors first
     if ( isset($districts['error']) ) {
       $addressesWithErrors++;
-      electoral_district_address_errors($districts, $contactAddresses->id);
+      electoral_district_address_errors($districts, $contactAddresses->id, $url);
     //Process divisions
     } else {
       $countyDivision = strtolower("ocd-division/country:us/state:$stateProvinceAbbrev");
@@ -286,7 +286,7 @@ function google_civic_information_city_districts($level, $limit, $update) {
     //Check for errors first
     if ( isset($districts['error']) ) {
       $addressesWithErrors++;
-      electoral_district_address_errors($districts, $contactAddresses->id);
+      electoral_district_address_errors($districts, $contactAddresses->id, $url);
     //Process divisions
     } else {
       $cityDivision = strtolower("ocd-division/country:us/state:$stateProvinceAbbrev");
@@ -394,7 +394,9 @@ function electoral_district_addresses($limit, $level, $statesProvinces, $update)
 /*
  * Helper function to save address errors when they occur
  */
-function electoral_district_address_errors($districts, $addressId) {
+function electoral_district_address_errors($districts, $addressId, $url) {
+  $errorSummary = "{$districts['error']['code']}, {$districts['error']['errors'][0]['reason']}, {$districts['error']['message']}";
+  CRM_Core_Error::debug_log_message("Electoral: For address id {$addressId}, Google API returned error: $errorSummary; for url $url");
   //Retain the error, so we can filter out the address on future runs until it's corrected
   $address_error_create = civicrm_api3('CustomValue', 'create', [
     'entity_id' => $addressId,
